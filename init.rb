74238d895d227ae53fa6394ec11dcababec7c5fe
Peng Zuo <cn.peng.zuo@gmail.com>
2009-05-14 00:56:12 +0800

    reload user patch in development mode

--- init.rb
+++ init.rb
@@ -1,10 +1,16 @@
-require 'redmine'
-require_dependency 'issue_summary_listener'
+require 'redmine' 
+require 'dispatcher'
+require_dependency 'issue_summary_listener'   
+require_dependency 'user'
+require 'user_patch'
 
-Redmine::Plugin.register :redmine_issue_summary do
+Redmine::Plugin.register :redmine_ez_summary do
   name 'Redmine Issue Summary plugin'
   author 'Peng Zuo'
   description 'Send Issue Summary Emails'
   version '0.0.1'
 end
-                     
+
+Dispatcher.to_prepare do                        
+	User.send(:include, PengZuo::UserPatch)
+end
