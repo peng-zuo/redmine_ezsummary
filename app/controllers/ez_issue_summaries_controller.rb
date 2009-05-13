3951a90bf4fba2c854f69496bd8506f1f95cb4c7
Peng Zuo <cn.peng.zuo@gmail.com>
2009-05-14 00:58:11 +0800

    move logic to model

--- app/controllers/ez_issue_summaries_controller.rb
+++ app/controllers/ez_issue_summaries_controller.rb
@@ -3,29 +3,38 @@ class EzIssueSummariesController < ApplicationController
 	helper :ez_issue_summaries
 	include EzIssueSummariesHelper
 	before_filter :find_issue, :only => [:show]
-	
-	def show    
+	                                                  
+	layout "ez_base"
+	def show                                 
+		@templates = User.current.ez_mail_templates    
 		respond_to do |format|
 			format.html { }
 		end
 	end
 	                                          
 	def email
+		find_issue(params[:issue_id])
 		email_content = params[:content]
-		email_recv = params[:recipients].split(/,|\s/)  
-		EzContact.transaction do 
-			email_recv.each do |recv|
-				EzContact.create(:email => recv)
-			end
+		email_recv = params[:recipients].split(/,|\s/)
+		
+#		@journals = @issue.journals.find(:all, :include => [:user, :details], :order => "#{Journal.table_name}.created_on ASC")
+#    @journals.each_with_index {|j,i| j.indice = i+1}
+#    @journals.reverse! if User.current.wants_comments_in_reverse_order?
+		
+	  
+		EzContact.import_contacts(email_recv)
+		
+		if(params[:save_flag] == "1")
+			User.current.ez_mail_templates.create(:content => email_content)
 		end
 		
-		    begin
-		EzIssueMailer.deliver_issue_summary(email_recv, params[:subject], email_content)
-	rescue   Exception => e
-		puts e.to_s
-	end
-		flash[:notice] = "succ delivered"
-		redirect_to "/issues"
-
+		begin
+			EzIssueMailer.deliver_issue_summary(email_recv, params[:subject], email_content, @issue)
+			flash[:notice] = t('ez.issue_mail_succ')  
+			redirect_to url_for(:controller => "issues", :action => "show", :id => @issue)
+	 	rescue
+			flash[:error] = t("ez.issue_mail_error")
+			render :action => "show"
+		end
 	end
 end
--- app/models/ez_contact.rb
+++ app/models/ez_contact.rb
@@ -1,5 +1,16 @@
 class EzContact < ActiveRecord::Base
-
+	def self.import_contacts(contacts)
+		begin
+			transaction do 
+				contacts.each do |contact|
+				  create(:email => contact)
+				end
+			end
+		rescue Exception => e
+			# logger.info e.to_s
+		end
+		
+	end
 	validates_presence_of :email
 	validates_uniqueness_of :email
 end
