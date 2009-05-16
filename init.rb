48bd9f3b8d3a2909762a06361756295abf483ef6
Peng Zuo <cn.peng.zuo@gmail.com>
2009-05-16 14:47:43 +0800

    add permissions

--- init.rb
+++ init.rb
@@ -3,14 +3,20 @@ require 'dispatcher'
 require_dependency 'issue_summary_listener'   
 require_dependency 'user'
 require 'user_patch'
+require 'action_mailer_patch'
 
 Redmine::Plugin.register :redmine_ez_summary do
   name 'Redmine Issue Summary plugin'
   author 'Peng Zuo'
   description 'Send Issue Summary Emails'
   version '0.0.1'
-end
 
+  project_module :issue_tracking do
+    permission :ez_send_mail, :ez_issue_summaries =>  [:show, :email]
+  end
+  
+end
 Dispatcher.to_prepare do                        
 	User.send(:include, PengZuo::UserPatch)
 end
+ActionMailer::Base.__send__ :include, PengZuo::ActionMailerPatch
