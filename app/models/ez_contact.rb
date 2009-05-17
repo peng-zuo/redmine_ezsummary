dc8b79ca1c63347ab68c31353ed7532de8ee7a42
Peng Zuo <cn.peng.zuo@gmail.com>
2009-05-18 01:53:51 +0800

    more validations

--- app/models/ez_contact.rb
+++ app/models/ez_contact.rb
@@ -18,4 +18,5 @@ class EzContact < ActiveRecord::Base
 
   validates_uniqueness_of :email
   validates_format_of :email, :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i
+  validates_length_of :email, :maximum => 60
 end
