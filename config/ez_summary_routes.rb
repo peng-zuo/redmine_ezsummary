e79aeebc656311be14c9f061f313fa6bfb4d2fc8
Peng Zuo <cn.peng.zuo@gmail.com>
2009-05-18 01:40:16 +0800

    change routes location

--- config/ez_summary_routes.rb
+++
@@ -1,4 +0,0 @@
-ActionController::Routing::Routes.draw do |map|    
-	map.resources :issue_summaries
-	map.resource :contacts, :controller => "contacts"
-end
--- init.rb
+++ init.rb
@@ -3,7 +3,6 @@ require 'dispatcher'
 require_dependency 'issue_summary_listener'   
 require_dependency 'user'
 require 'user_patch'
-require 'action_mailer_patch'
 
 Redmine::Plugin.register :redmine_ez_summary do
   name 'Redmine Issue Summary plugin'
@@ -14,9 +13,9 @@ Redmine::Plugin.register :redmine_ez_summary do
   project_module :issue_tracking do
     permission :ez_send_mail, :ez_issue_summaries =>  [:new, :create]
   end
-  
+
 end
+
 Dispatcher.to_prepare do                        
 	User.send(:include, PengZuo::UserPatch)
 end
-#ActionMailer::Base.__send__ :include, PengZuo::ActionMailerPatch
