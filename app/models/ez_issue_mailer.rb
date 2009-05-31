6abf8178750fe3df43463650f166ff1ef6f06bac
Peng Zuo <cn.peng.zuo@gmail.com>
2009-05-31 20:52:22 +0800

    bug fix: missing template bug. explicitly set content-type to alternative.

--- app/models/ez_issue_mailer.rb
+++ app/models/ez_issue_mailer.rb
@@ -1,40 +1,40 @@
-class EzIssueMailer < ActionMailer::Base
-  helper :custom_fields
-  helper :application
-  helper :issues
+class EzIssueMailer < Mailer #ActionMailer::Base
   helper :ez_issue_summaries
-  include Redmine::I18n
 
-  self.template_root = ActionController::Base.view_paths.first
+  # self.template_root = ActionController::Base.view_paths.first
 
-  include IssuesHelper
-  include CustomFieldsHelper
-  include ApplicationHelper
   include EzIssueSummariesHelper
 
-  def self.default_url_options
-    Mailer.default_url_options
-  end
 
 
   def issue_summary(recv, thesubject, content, issue, journals)
+    redmine_headers 'Project' => issue.project.identifier,
+                    'Issue-Id' => issue.id,
+                    'Issue-Author' => issue.author.login
+    
     recipients recv
     subject thesubject
-    sent_on Time.now
-
     body    :issue => issue,
             :content => content,
             :issue_url => url_for(:controller => 'issues', :action => 'show', :id => issue),
             :journals => journals
-    
+    content_type "multipart/alternative"
+
+    part "text/plain" do |p|
+      p.body = render_message("issue_summary.text.plain.rhtml", body)
+    end
+
+    part "text/html" do |p|
+      p.body = render_message("issue_summary.text.html.rhtml", body)
+    end            
   end
 
-  def render_message(method_name, body)
-    layout = method_name.to_s.match(%r{text\.(html|xml)}) ? 'layout.text.html.rhtml' : 'layout.text.plain.rhtml'
-
-    body[:content_for_layout] = render(:file => method_name, :body => body  )
-    root_view_path = File.join( RAILS_ROOT, "app", "views")
-    ActionView::Base.new( root_view_path, body, self).render(:file => "mailer/#{layout}", :use_full_path => true)
-  end
+#  def render_message(method_name, body)
+#    layout = method_name.to_s.match(%r{text\.(html|xml)}) ? 'layout.text.html.rhtml' : 'layout.text.plain.rhtml'
+#
+#    body[:content_for_layout] = render(:file => method_name, :body => body  )
+#    root_view_path = File.join( RAILS_ROOT, "app", "views")
+#    ActionView::Base.new( root_view_path, body, self).render(:file => "mailer/#{layout}", :use_full_path => true)
+#  end
 
 end                               
