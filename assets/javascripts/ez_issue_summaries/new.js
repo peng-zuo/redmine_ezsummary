989b3d4a8a354a0d4bebbdd7802c8e5b456c08fa
Peng Zuo <cn.peng.zuo@gmail.com>
2009-06-02 00:40:11 +0800

    added style for required input fields

--- assets/javascripts/ez_issue_summaries/new.js
+++ assets/javascripts/ez_issue_summaries/new.js
@@ -1,13 +1,21 @@
 var $j = jQuery.noConflict();
+
+function fill_in(){
+    var textarea = $j("#mail_content");
+    var selected = $j("#templates option:selected");
+    if (selected.val()) {
+        textarea.val(textarea.val() + selected.val());
+    }
+}
+
+function required_field(){
+    $j("label.required").css("color", "rgb(72, 72, 72)").append($j("<span class='required'>*</span>"));
+}
+
 jQuery(document).ready(function() {
     $j("#recipients").autocomplete("/ez_contacts", {
         multiple: true
     });
-    $j("#fill_in").click(function() {
-        var textarea = $j("#mail_content");
-        var selected = $j("#templates option:selected");
-        if (selected.val()) {
-            textarea.val(textarea.val() + selected.val());
-        }
-    });
+    $j("#fill_in").click( fill_in );
+    required_field();
 });
