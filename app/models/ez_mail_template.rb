bc0b7cb978c3e04578c8d7b643001c4b7c1457bf
Peng Zuo <cn.peng.zuo@gmail.com>
2009-06-01 22:37:41 +0800

    bug fix: uniqueness of template content is now scoped with user_id

--- app/models/ez_mail_template.rb
+++ app/models/ez_mail_template.rb
@@ -2,6 +2,6 @@ class EzMailTemplate < ActiveRecord::Base
   belongs_to :users
 
   validates_presence_of :content
-  validates_uniqueness_of :content
+  validates_uniqueness_of :content, :scope => :user_id
   validates_presence_of :user_id
 end
